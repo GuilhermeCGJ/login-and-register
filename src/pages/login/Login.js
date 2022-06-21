0671eab4d74387ca999860a802348fc3af84124a
Guilherme Carvalho <guil_car@hotmail.com>
2022-06-21 19:40:05 -0300

    base do formulário criada

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,8 +1,40 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 function Login () {
   return (
-    <div>Login</div>
+    <section id="login-page">
+      <form>
+        <div>
+          <div>
+            <input
+              name="user"
+              type="email"
+              placeholder="Digite seu e-mail"
+              required
+            />
+          </div>
+          <div>
+            <input
+              name="password"
+              type="password"
+              placeholder="Digite sua senha"
+              required
+            />
+          </div>
+          <div>
+            <button
+              type="button"
+            >
+              Logar
+            </button>
+          </div>
+        </div>
+      </form>
+      <p>
+        Ainda não tem uma conta? <Link to="/register">Cadastre-se</Link>
+      </p>
+    </section>
   )
 };
 
--- src/pages/register/Register.js
+++ src/pages/register/Register.js
@@ -1,8 +1,86 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 function Register () {
   return (
-    <div>Register</div>
+    <section id="login-page">
+      <form>
+        <div>
+        <div>
+            <h5>Nome</h5>
+            <input
+              name="name"
+              type="name"
+              placeholder="Digite seu nome"
+              required
+            />
+          </div>
+          <div>
+          <h5>Sobrenome</h5>
+            <input
+              name="surname"
+              type="surname"
+              placeholder="Digite seu sobrenome"
+              required
+            />
+          </div>
+          <div>
+            <h5>Usuário</h5>
+            <input
+              name="user"
+              type="user"
+              placeholder="Digite seu nome de usuário"
+              required
+            />
+          </div>
+          <div>
+            <h5>Email</h5>
+            <input
+              name="email"
+              type="email"
+              placeholder="Digite seu email"
+              required
+            />
+          </div>
+          <div>
+            <h5>Data de Nascimento</h5>
+            <input
+              type='birthdate'
+              name='birthdate'
+              placeholder='Sua data de nascimento'
+            />
+          </div>
+          <div>
+            <h5>Email</h5>
+            <input
+              name="password"
+              type="password"
+              placeholder="Digite uma senha"
+              required
+            />
+          </div>
+          <div>
+            <h5>Repita seu Email</h5>
+            <input
+              name="confirm-password"
+              type="confirm-password"
+              placeholder="Repita sua senha"
+              required
+            />
+          </div>
+          <div>
+            <button
+              type="button"
+            >
+              Cadastrar
+            </button>
+          </div>
+        </div>
+      </form>
+      <p>
+        Já tem uma conta? <Link to="/">Logar</Link>
+      </p>
+    </section>
   )
 }
 
